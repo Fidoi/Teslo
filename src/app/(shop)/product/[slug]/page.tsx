0bd58db7433d94f237a5297cecfd20bf50872259
Fidoi <elautoricoi@gmail.com>
2024-12-11 02:00:00 -0300

    Segundo commit

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -27,7 +27,7 @@ export async function generateMetadata(
   const product = await getProductBySlug(slug);
 
   // optionally access and extend (rather than replace) parent metadata
-  //const previousImages = (await parent).openGraph?.images || [];
+  // const previousImages = (await parent).openGraph?.images || []
 
   return {
     title: product?.title ?? 'Producto no encontrado',
@@ -35,43 +35,54 @@ export async function generateMetadata(
     openGraph: {
       title: product?.title ?? 'Producto no encontrado',
       description: product?.description ?? '',
+      // images: [], // https://misitioweb.com/products/image.png
       images: [`/products/${product?.images[1]}`],
     },
   };
 }
-export default async function ProductPage({ params }: Props) {
+
+export default async function ProductBySlugPage({ params }: Props) {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
+  console.log(product);
 
   if (!product) {
     notFound();
   }
+
   return (
     <div className='mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3'>
-      {/* SlideShow */}
-      <div className='col-span-1 md:col-span-2'>
-        {/* Mobile*/}
+      {/* Slideshow */}
+      <div className='col-span-1 md:col-span-2 '>
+        {/* Mobile Slideshow */}
         <ProductMobileSlideshow
           title={product.title}
           images={product.images}
           className='block md:hidden'
         />
-        {/* Desktop */}
+
+        {/* Desktop Slideshow */}
         <ProductSlideshow
           title={product.title}
           images={product.images}
           className='hidden md:block'
         />
       </div>
+
       {/* Detalles */}
-      <div className='col-span-1 px-5 '>
+      <div className='col-span-1 px-5'>
         <StockLabel slug={product.slug} />
-        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
           {product.title}
         </h1>
+
         <p className='text-lg mb-5'>${product.price}</p>
+
         <AddToCart product={product} />
-        <h3 className='font-bold text-sm'>Descripcion</h3>
+
+        {/* Descripción */}
+        <h3 className='font-bold text-sm'>Descripción</h3>
         <p className='font-light'>{product.description}</p>
       </div>
     </div>
